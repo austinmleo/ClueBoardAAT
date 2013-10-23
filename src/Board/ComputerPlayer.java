5aa76c8c585f59ed5990c02f8ac54d795fae6b46
Sean Zeiler <szeiler@mines.edu>
2013-10-23 17:46:51 -0600

    added the computer player class.

---
+++ src/Board/ComputerPlayer.java
@@ -0,0 +1,16 @@
+package Board;
+
+import java.util.ArrayList;
+
+public class ComputerPlayer extends Player {
+
+	public ComputerPlayer(String name, String color, int currentIndex,
+			ArrayList<Card> possibleWeapons, ArrayList<Card> possiblePeople,
+			ArrayList<Card> possibleRooms) {
+		super(name, color, currentIndex, possibleWeapons, possiblePeople, possibleRooms);
+	}
+	
+	
+	
+	
+}
