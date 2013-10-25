d2e3c982d687dbbcd5bc6ce6582e5c0155e947c2
Todd Sorden <tsorden@mines.edu>
2013-10-25 14:25:23 -0600

    fixed boarder problem, now must fix space bettween the buttons and the dark_gray background of the text area

--- src/control_GUI/Control_GUI.java
+++ src/control_GUI/Control_GUI.java
@@ -11,6 +11,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JTextArea;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
@@ -24,125 +25,143 @@ import javax.swing.border.TitledBorder;
 
 
 public class Control_GUI extends JFrame {
-	
+
+	// whoseTurnPanel variables
 	private String playersName;
 	private JTextArea displayPlayer;
-	
+
+	// buttonPanel variables
 	private JButton nextPlayerButton, accusationButton;
-	
+
+	//diePanel variables
 	private String dieRoll;
 	private JTextArea die;
-	
+
+	//guessPanel variables
 	private String makeAGuess;
 	private JTextArea inputGuess;
-	
+
+	//resultPanel variables
 	private String response;
 	private JTextArea responseArea;
-	
-		public Control_GUI(){
-			
-			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			setTitle("Clue GUI");
-			setSize(800, 200);	
-			
-		
-		
-			// Whose Turn Label and Current Players Turn
-			
-			JPanel whoseTurnPanel = new JPanel();
-			JLabel turnLabel = new JLabel("Whose Turn");
-			playersName = "Todd";
-			displayPlayer = new JTextArea(1,10);
-			displayPlayer.setBackground(Color.lightGray);
-			displayPlayer.setEditable(false);
-			displayPlayer.setLineWrap(true);
-			
-			whoseTurnPanel.add(turnLabel);
-			whoseTurnPanel.add(displayPlayer);
-			
-			add(whoseTurnPanel, BorderLayout.NORTH);
-			
-			
-			// Buttons
-			
-			JPanel buttonPanel = new JPanel();
-			nextPlayerButton = new JButton("Next Player");
-			accusationButton = new JButton("Make and accusation");
-			
-			buttonPanel.add(nextPlayerButton);
-			buttonPanel.add(accusationButton);
-			add(buttonPanel, BorderLayout.SOUTH);
-			
-			// Die
-			
-			JPanel diePanel = new JPanel();
-			JLabel dieLabel = new JLabel("Die");
-			die = new JTextArea(1,10);
-			die.setBackground(Color.lightGray);
-			die.setEditable(false);
-			die.setLineWrap(true);
-			
-			Random randomDie = new Random();
-			int tempDie = (randomDie.nextInt(5) + 1);
-			dieRoll = Integer.toString(tempDie);
-			diePanel.add(dieLabel);
-			diePanel.add(die);
-			add(diePanel, BorderLayout.WEST);
-			diePanel.setBorder(new TitledBorder (new EtchedBorder(), "Die"));
-			
-			// Guess
-			
-			JPanel guessPanel = new JPanel();
-			JLabel guessLabel = new JLabel("Guess");
-			makeAGuess = "Plasma Gun";
-			inputGuess = new JTextArea(1,10);
-			inputGuess.setBackground(Color.lightGray);
-			inputGuess.setEditable(false);
-			inputGuess.setLineWrap(true);
-			
-			
-			
-			guessPanel.add(guessLabel);
-			guessPanel.add(inputGuess);
-			
-			add(guessPanel, BorderLayout.CENTER);
-			guessPanel.setBorder(new TitledBorder (new EtchedBorder(), "Guess"));
-			
-			
-			// Guess Result
-			JPanel resultPanel = new JPanel();
-			JLabel resultLabel = new JLabel("Response");
-			response = "I have nothing";
-			responseArea = new JTextArea(1,10);
-			responseArea.setBackground(Color.lightGray);
-			responseArea.setEditable(false);
-			responseArea.setLineWrap(true);
-			
-			resultPanel.add(resultLabel);
-			resultPanel.add(responseArea);
-			
-			add(resultPanel, BorderLayout.EAST);
-			resultPanel.setBorder(new TitledBorder (new EtchedBorder(), "Guess Reuslt"));
-			
-			
-			
-			updateDisplay();
-			
-			
-		}
-		
-		private void updateDisplay	(){
-			die.setText(dieRoll);
-			displayPlayer.setText(playersName);
-			inputGuess.setText(makeAGuess);
-			responseArea.setText(response);
-		}
-		
 
+	public Control_GUI(){
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Clue GUI");
+		setSize(800, 200);	
+
+		final JPanel test = new JPanel();
+
+		GridLayout experimentLayout = new GridLayout(0,3);
+		test.setLayout(experimentLayout);
+
+
+
+		// Whose Turn Label and Current Players Turn
+
+		JPanel whoseTurnPanel = new JPanel();
+		JLabel turnLabel = new JLabel("Whose Turn");
+		playersName = "Todd";
+		displayPlayer = new JTextArea(1,10);
+
+		displayPlayer.setBackground(Color.lightGray);
+		displayPlayer.setEditable(false);
+		displayPlayer.setLineWrap(true);
+		whoseTurnPanel.add(turnLabel);
+		whoseTurnPanel.add(displayPlayer);
+		//add(whoseTurnPanel, BorderLayout.NORTH);
+
+
+		// Buttons
+
+		JPanel buttonPanel = new JPanel();
+		nextPlayerButton = new JButton("Next Player");
+		buttonPanel.add(nextPlayerButton);
+		//add(buttonPanel, BorderLayout.SOUTH);
+
+		JPanel buttonPanel2 = new JPanel();
+		accusationButton = new JButton("Make and accusation");
+		buttonPanel2.add(accusationButton);
+
+
+		// Die
+
+		JPanel diePanel = new JPanel();
+		JLabel dieLabel = new JLabel("Die");
+		die = new JTextArea(1,10);
+		die.setBackground(Color.lightGray);
+		die.setEditable(false);
+		die.setLineWrap(true);
+		Random randomDie = new Random();
+		int tempDie = (randomDie.nextInt(5) + 1);
+		dieRoll = Integer.toString(tempDie);
+		diePanel.add(dieLabel);
+		diePanel.add(die);
+		//add(diePanel, BorderLayout.WEST);
+		
+		diePanel.setBorder(new TitledBorder (new EtchedBorder(), "Die"));
 	
-		public static void main(String[] args) {
-			Control_GUI controler = new Control_GUI() ;
-			controler.setVisible(true);
-			
-		}
+
+
+		// Guess
+
+		JPanel guessPanel = new JPanel();
+		JLabel guessLabel = new JLabel("Guess");
+		makeAGuess = "Plasma Gun";
+		inputGuess = new JTextArea(1,10);
+		inputGuess.setBackground(Color.lightGray);
+		inputGuess.setEditable(false);
+		inputGuess.setLineWrap(true);
+		guessPanel.add(guessLabel);
+		guessPanel.add(inputGuess);
+		//add(guessPanel, BorderLayout.CENTER);
+		guessPanel.setBorder(new TitledBorder (new EtchedBorder(), "Guess"));
+
+
+		// Guess Result
+		JPanel resultPanel = new JPanel();
+		JLabel resultLabel = new JLabel("Response");
+		response = "I have nothing";
+		responseArea = new JTextArea(1,10);
+		responseArea.setBackground(Color.lightGray);
+		responseArea.setEditable(false);
+		responseArea.setLineWrap(true);
+		resultPanel.add(resultLabel);
+		resultPanel.add(responseArea);
+		//add(resultPanel, BorderLayout.EAST);
+		resultPanel.setBorder(new TitledBorder (new EtchedBorder(), "Guess Reuslt"));
+
+
+		// Allows text to be updated and dispalyed
+		updateDisplay();
+
+		// add panels to the grid
+		test.add(whoseTurnPanel);
+		test.add(buttonPanel);
+
+		test.add(buttonPanel2);
+		test.add(diePanel);
+		test.add(guessPanel);
+		test.add(resultPanel);
+
+
+		add(test, BorderLayout.SOUTH);
+		//add(new JSeparator(), BorderLayout.CENTER);
+	}
+
+	private void updateDisplay	(){
+		die.setText(dieRoll);
+		displayPlayer.setText(playersName);
+		inputGuess.setText(makeAGuess);
+		responseArea.setText(response);
+	}
+
+
+
+	public static void main(String[] args) {
+		Control_GUI controler = new Control_GUI() ;
+		controler.setVisible(true);
+
+	}
 }
