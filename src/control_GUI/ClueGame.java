17cfd7847b56c629823307e6a8f5c505b2481515
Sean Zeiler <szeiler@mines.edu>
2013-10-29 23:27:12 -0600

    added in the ClueGame class

---
+++ src/control_GUI/ClueGame.java
@@ -0,0 +1,68 @@
+package control_GUI;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+import Board.Board;
+
+public class ClueGame extends JFrame {
+
+	private Board board;
+	
+    public ClueGame() {
+        setTitle("Clue Game");
+        setSize(800, 700);
+        
+        createMenuBar();
+        createBoard();
+        createControls();
+        
+    }
+	
+	public void createMenuBar() {}
+	
+    public void createBoard() {
+        board = new Board();
+        add(board, BorderLayout.CENTER);
+    }
+	
+    public void createControls() {}
+	
+	
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public Board getBoard() {
+        return board;
+    }
+}
