bd921985f15bb69cb66abb4d04248516ee532129
Todd Sorden <tsorden@mines.edu>
2013-10-31 12:00:23 -0600

    fixed cluegame class

--- src/control_GUI/ClueGame.java
+++ src/control_GUI/ClueGame.java
@@ -1,68 +1,90 @@
 package control_GUI;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 import Board.Board;
 
 public class ClueGame extends JFrame {
 
 	private Board board;
+	private Control_GUI gui;
 	
     public ClueGame() {
         setTitle("Clue Game");
         setSize(800, 700);
         
-       // createMenuBar();
+        createMenuBar();
         createBoard();
-       // createControls();
+        createControls();
         
     }
 	
-	public void createMenuBar() {}
+	public void createMenuBar() {
+		
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		menuBar.add(openNotes());
+		
+		
+	}
 	
     public void createBoard() {
         board = new Board();
         add(board, BorderLayout.CENTER);
     }
 	
-    public void createControls() {}
+    public void createControls() {
+    	gui = new Control_GUI();
+    	add(gui, BorderLayout.SOUTH);
+    	
+    }
 	
-	
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
     
     public Board getBoard() {
         return board;
     }
+    public Control_GUI getGui(){
+    	return gui;
+    }
+    
+    public static void main(String[] args) {
+    	ClueGame controler = new ClueGame() ;
+		controler.setVisible(true);
+
+	}
+    
+    private JMenu openNotes(){
+		JMenu menu = new JMenu("File");
+		menu.add(detectiveNotes());
+		return menu;
+	}
+	
+	private JMenuItem detectiveNotes(){
+		JMenuItem item = new JMenuItem("Detective Notes");
+		class MenuItemListener  implements ActionListener{
+			public void actionPerformed(ActionEvent e){
+				DetectiveNotes notes = new DetectiveNotes();
+				notes.setVisible(true);
+			
+			}
+
+			
+			
+		}
+		item.addActionListener(new MenuItemListener());
+		return item;
+		
+	}
+    
 }
+
